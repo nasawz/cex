8f9b92ab78814a61b0f40bc009824338059e0f74
王喆 <nasa127@gmail.com>
2016-09-16 18:09:48 +0800

    float button

--- example/app.js
+++ example/app.js
@@ -178,93 +178,104 @@ import ReactDOM from 'react-dom'
 // )
 
 // ------------------
-import Card from '../src/components/card/card.jsx'
-import CardContent from '../src/components/card/card-content.jsx'
-import CardHeader from '../src/components/card/card-header.jsx'
-import CardFooter from '../src/components/card/card-footer.jsx'
-import CardImage from '../src/components/card/card-image.jsx'
-import CardTitle from '../src/components/card/card-title.jsx'
-import CardTools from '../src/components/card/card-tools.jsx'
-import List from '../src/components/list/list.jsx'
-import Item from '../src/components/list/item.jsx'
-import ItemMedia from '../src/components/list/item-media.jsx'
-import ItemTitle from '../src/components/list/item-title.jsx'
-import ItemContent from '../src/components/list/item-content.jsx'
-import ItemSubtitle from '../src/components/list/item-sub-title.jsx'
-import ItemText from '../src/components/list/item-text.jsx'
-import ItemTitleRow from '../src/components/list/item-title-row.jsx'
-import ItemTitleAfter from '../src/components/list/item-title-after.jsx'
-import IconButton from '../src/components/icon/icon-button.jsx'
+// import Card from '../src/components/card/card.jsx'
+// import CardContent from '../src/components/card/card-content.jsx'
+// import CardHeader from '../src/components/card/card-header.jsx'
+// import CardFooter from '../src/components/card/card-footer.jsx'
+// import CardImage from '../src/components/card/card-image.jsx'
+// import CardTitle from '../src/components/card/card-title.jsx'
+// import CardTools from '../src/components/card/card-tools.jsx'
+// import List from '../src/components/list/list.jsx'
+// import Item from '../src/components/list/item.jsx'
+// import ItemMedia from '../src/components/list/item-media.jsx'
+// import ItemTitle from '../src/components/list/item-title.jsx'
+// import ItemContent from '../src/components/list/item-content.jsx'
+// import ItemSubtitle from '../src/components/list/item-sub-title.jsx'
+// import ItemText from '../src/components/list/item-text.jsx'
+// import ItemTitleRow from '../src/components/list/item-title-row.jsx'
+// import ItemTitleAfter from '../src/components/list/item-title-after.jsx'
+// import IconButton from '../src/components/icon/icon-button.jsx'
+//
+// ReactDOM.render(
+//     <div>
+//         <Card>
+//             <CardContent>
+//                 card
+//             </CardContent>
+//         </Card>
+//         <Card>
+//             <CardHeader>
+//                 header
+//             </CardHeader>
+//             <CardContent>
+//                 card
+//             </CardContent>
+//             <CardFooter>
+//                 footer
+//             </CardFooter>
+//         </Card>
+//         <Card>
+//             <CardHeader>
+//                 <CardTitle>Cex.js</CardTitle>
+//                 <CardTools>
+//                     <IconButton icon='icon-add_a_photo' />
+//                 </CardTools>
+//             </CardHeader>
+//             <CardContent>
+//                 card
+//             </CardContent>
+//             <CardFooter>
+//                 <IconButton icon='icon-favorite'>喜欢</IconButton>
+//                 <IconButton icon='icon-chat'>评论</IconButton>
+//                 <IconButton icon='icon-share'>分享</IconButton>
+//             </CardFooter>
+//         </Card>
+//         <Card>
+//             <CardImage src='http://ces00.b0.upaiyun.com/2016/07/27/upload_3184ba6f69a11468104cc90c2c546b43.png!480x174' >
+//                 <CardTitle>微家</CardTitle>
+//             </CardImage>
+//             <CardContent>
+//                 card
+//             </CardContent>
+//         </Card>
+//         <Card>
+//             <CardContent>
+//                 <List>
+//                     <Item>
+//                         <ItemMedia>
+//                             <img src="http://ugc.qpic.cn/gbar_pic/PYnx3ZdPvsTiaj9aNylwCQbvWIcE2J9lRBkUxb5Sx1s9H7AqhrKx3zw/"/>
+//                         </ItemMedia>
+//                         <ItemContent>
+//                             <ItemTitleRow>
+//                                 <ItemTitle>德玛西亚</ItemTitle>
+//                                 <ItemTitleAfter>2014-12-23</ItemTitleAfter>
+//                             </ItemTitleRow>
+//                             <ItemSubtitle>
+//                                 网通二区
+//                             </ItemSubtitle>
+//                             <ItemText>
+//                                 德玛西亚，原为著名竞技游戏英雄联盟中的一个势力，也是中国区一个服务器（网通二区）的名称。
+//                             </ItemText>
+//                         </ItemContent>
+//                     </Item>
+//                 </List>
+//             </CardContent>
+//             <CardFooter>
+//                 <IconButton icon='icon-favorite'>喜欢</IconButton>
+//                 <IconButton icon='icon-chat'>评论</IconButton>
+//             </CardFooter>
+//         </Card>
+//     </div>,
+//     document.getElementById('container')
+// )
+
+// ------------------
+import FloatButton from '../src/components/button/float-buttom.jsx'
+
 
 ReactDOM.render(
     <div>
-        <Card>
-            <CardContent>
-                card
-            </CardContent>
-        </Card>
-        <Card>
-            <CardHeader>
-                header
-            </CardHeader>
-            <CardContent>
-                card
-            </CardContent>
-            <CardFooter>
-                footer
-            </CardFooter>
-        </Card>
-        <Card>
-            <CardHeader>
-                <CardTitle>Cex.js</CardTitle>
-                <CardTools>
-                    <IconButton icon='icon-add_a_photo' />
-                </CardTools>
-            </CardHeader>
-            <CardContent>
-                card
-            </CardContent>
-            <CardFooter>
-                <IconButton icon='icon-favorite'>喜欢</IconButton>
-                <IconButton icon='icon-chat'>评论</IconButton>
-                <IconButton icon='icon-share'>分享</IconButton>
-            </CardFooter>
-        </Card>
-        <Card>
-            <CardImage src='http://ces00.b0.upaiyun.com/2016/07/27/upload_3184ba6f69a11468104cc90c2c546b43.png!480x174' >
-                <CardTitle>微家</CardTitle>
-            </CardImage>
-            <CardContent>
-                card
-            </CardContent>
-        </Card>
-        <Card>
-            <CardContent>
-                <List>
-                    <Item>
-                        <ItemMedia>
-                            <img src="http://ugc.qpic.cn/gbar_pic/PYnx3ZdPvsTiaj9aNylwCQbvWIcE2J9lRBkUxb5Sx1s9H7AqhrKx3zw/"/>
-                        </ItemMedia>
-                        <ItemContent>
-                            <ItemTitleRow>
-                                <ItemTitle>德玛西亚</ItemTitle>
-                                <ItemTitleAfter>2014-12-23</ItemTitleAfter>
-                            </ItemTitleRow>
-                            <ItemSubtitle>
-                                网通二区
-                            </ItemSubtitle>
-                            <ItemText>
-                                德玛西亚，原为著名竞技游戏英雄联盟中的一个势力，也是中国区一个服务器（网通二区）的名称。
-                            </ItemText>
-                        </ItemContent>
-                    </Item>
-                </List>
-            </CardContent>
-            <CardFooter>
-                <IconButton icon='icon-favorite'>喜欢</IconButton>
-                <IconButton icon='icon-chat'>评论</IconButton>
-            </CardFooter>
-        </Card>
+        <FloatButton icon='icon-add_a_photo' fixed/>
     </div>,
     document.getElementById('container')
 )
---
+++ src/components/button/float-buttom.jsx
@@ -0,0 +1,36 @@
+/**
+ * create by nasa.wang
+ */
+
+import React from 'react'
+import classNames from 'classnames'
+
+import Icon from '../icon/icon.jsx'
+
+import './float-button.less'
+
+
+const FloatButton = React.createClass({
+    getDefaultProps() {
+        return {
+            size: 22,
+            color: '#777',
+            fixed: true
+        }
+    },
+    render () {
+        let {style,size,icon,color,fixed} = this.props
+        let colorstr = 'cex-float-button-'+color
+        let classes = {
+            'cex-float-button' : true,
+            'cex-float-button-fixed' : fixed,
+        }
+        return (
+            <a className={classNames(this.props.className,classes,colorstr)} style={style}>
+                <Icon icon={icon} color='#FFF' size={size}></Icon>
+            </a>
+        )
+    }
+})
+
+export default FloatButton
