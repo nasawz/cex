e5bf8ea7aa8cdc222d1731bc3de182ec94a03d27
吴丹丹 <danw@kailuzhe.cn>
2016-09-30 15:12:35 +0800

    fix

--- example/modules/cex/views/vuploader.jsx
+++ example/modules/cex/views/vuploader.jsx
@@ -6,13 +6,20 @@ import GalleryItem from 'cex/components/image/gallery-item.jsx'
 
 
 const VUploader = React.createClass({
+    onSelectImg(file) {
+        console.log("-------")
+        console.log(file)
+    },
     render() {
+        let _style = {
+            width: '25%'
+        }
         return (
             <div>
                 <Gallery>
-                    <GalleryItem className='cex-uploader-image' src='http://ces00.b0.upaiyun.com/2016/09/17/upload_6f046a3cba5894c8deb72fd1b568021c.jpg!small'/>
-                    <GalleryItem src='http://ces00.b0.upaiyun.com/2016/09/17/upload_6f046a3cba5894c8deb72fd1b568021c.jpg!small'/>
-                   <Uploader />
+                    <GalleryItem style={_style} src='http://ces00.b0.upaiyun.com/2016/09/17/upload_6f046a3cba5894c8deb72fd1b568021c.jpg!small'/>
+                    <GalleryItem style={_style}src='http://ces00.b0.upaiyun.com/2016/09/17/upload_6f046a3cba5894c8deb72fd1b568021c.jpg!small'/>
+                   <Uploader style={_style} onSelectImg={this.onSelectImg}/>
                 </Gallery>
                 
             </div>
--- src/components/uploader/uploader.jsx
+++ src/components/uploader/uploader.jsx
@@ -3,8 +3,10 @@
  */
 
 import React from 'react'
+import ReactDOM from 'react-dom'
 
 import classNames from 'classnames'
+import merge from 'lodash/merge'
 
 
 import './uploader.less'
@@ -12,40 +14,37 @@ import './uploader.less'
 
 const Uploader = React.createClass({
     handelSelectFile(e) {
-        console.log(e.target.files[0])
+        let file = e.target.files[0]
+        this.props.onSelectImg(file)
 
-        
-        // if (){ 
-
-        // }
-        // let self = this;
-        // let file = e.target.files[0];
-        // let imageType = /image.*/
-        // if (!file.type.match(imageType)) {
-        //     return 0;
-        // }
-        // window.lrz(file, {
-        //     width: 640,
-        //     height: 1136
-        // })
-        //     .then(function (rst) {
-        //         // 处理成功会执行
-        //         self.onSelectImg(rst);
-        //     })
-        //     .catch(function (err) { })
-        //     .always(function () { });
-
+    },
+    getInitialState() {
+        return {
+            height: 0
+        }
+    },
+    componentDidMount() {
+        let self = this
+        setTimeout(function () {
+            self.el = ReactDOM.findDOMNode(self)
+            self.setState({
+                height: self.el.getBoundingClientRect().width
+            })
+        }, 500)
     },
     render() {
         let {style} = this.props
         let classes = {
             'cex-uploader': true,
-            'cex-uploader__input-box': true
+            'cex-uploader-input-box': true
+        }
+        let _style = {
+            height: this.state.height
         }
         return (
-            
-            <div className={classNames(this.props.className, classes) } style={style}>
-                <input className="cex-uploader__input" type="file" accept="image/*" multiple onChange={ this.handelSelectFile } />
+
+            <div className={classNames(this.props.className, classes) } style={merge({},_style,style)}>
+                <input className="cex-uploader-input" type="file" accept="image/*" multiple onChange={ this.handelSelectFile } />
             </div>
         )
     }
