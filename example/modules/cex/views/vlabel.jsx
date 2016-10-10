33507a83bbbfca5e69802ac0fa3ad300ea442f6d
王喆 <nasa127@gmail.com>
2016-10-10 14:33:06 +0800

    time label 完成

--- example/modules/cex/views/vlabel.jsx
+++ example/modules/cex/views/vlabel.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 import Divider from 'cex/components/divider/divider.jsx'
 import AddressLabel from 'cex/components/label/address-label.jsx'
 import ColorLabel from 'cex/components/label/color-label.jsx'
+import TimeLabel from 'cex/components/label/time-label.jsx'
 
 const VLabel = React.createClass({
     render () {
@@ -15,6 +16,10 @@ const VLabel = React.createClass({
                 <AddressLabel txt='北京，朝阳区，远洋国际中心' open={false}/>
                 <Divider>AddressLabel view mode</Divider>
                 <AddressLabel mode='view' txt='北京，朝阳区，远洋国际中心' />
+                <Divider>time</Divider>
+                <TimeLabel time='2016-08-11T11:58:15.524502Z' /><br />
+                <TimeLabel time='2016-010-9T11:58:15.524502Z' /><br />
+                <TimeLabel time='2016-010-10T11:58:15.524502Z' /><br />
             </div>
         )
     }
