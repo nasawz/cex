976b0293abdacf8a81551cef81e3476399c10569
吴丹丹 <danw@kailuzhe.cn>
2016-10-11 17:40:54 +0800

    fix

--- src/complex/form/post-form.jsx
+++ src/complex/form/post-form.jsx
@@ -19,46 +19,53 @@ import AddressLabel from '../../components/label/address-label.jsx'
 import './post-form.less'
 
 const PostForm = React.createClass({
-    changeSel2Photo(){
+    changeSel2Photo() {
         this.setState({
             sel: 'photo'
         })
     },
-    changeSel2Face(){
+    changeSel2Face() {
         this.setState({
             sel: 'face'
         })
     },
-    getDefaultProps(){
+    getDefaultProps() {
         return {
             color: '#02a882',
             sel: 'photo'
         }
     },
-    getInitialState(){
+    getInitialState() {
         return {
             sel: this.props.sel
         }
     },
-    render () {
+    doPublish() {
+        // let address
+        // let file
+        let data = {}
+        data.txt_content = this.refs.txt_content.value().trim()
+        this.props.onSubmit(data)
+    },
+    render() {
         let _col_photo = this.state.sel == 'photo' ? this.props.color : '#777'
         let _col_face = this.state.sel == 'face' ? this.props.color : '#777'
         let _styleUpload = {
             padding: '15px',
-            backgroundColor:'#e8e8e8',
-            minHeight:'200px',
-            display: this.state.sel == 'photo' ? 'block': 'none'
+            backgroundColor: '#e8e8e8',
+            minHeight: '200px',
+            display: this.state.sel == 'photo' ? 'block' : 'none'
         }
         let _emoticonsPicker = this.state.sel == 'face' ? (
             <EmoticonsPicker />
-        ): ''
+        ) : ''
         return (
             <div className='cex-post-form'>
-                <List style={{backgroundColor:'#e8e8e8'}}>
-                    <XTextArea placeholder='说点什么吧' max={20}  height={120} >
+                <List style={{ backgroundColor: '#e8e8e8' }}>
+                    <XTextArea ref='txt_content' placeholder='说点什么吧' max={20}  height={120} >
                         <AddressLabel />
                     </XTextArea>
-                    <ToolBar style={{paddingLeft:'0px'}}>
+                    <ToolBar style={{ paddingLeft: '0px' }}>
                         <BarToolsGroup>
                             <BarTools>
                                 <IconButton onClick={ this.changeSel2Photo } color={_col_photo} icon='icon-photo' />
@@ -67,11 +74,11 @@ const PostForm = React.createClass({
                                 <IconButton onClick={ this.changeSel2Face } color={_col_face} icon='icon-tag_faces' />
                             </BarTools>
                         </BarToolsGroup>
-                        <XButton mini type='primary'>发表</XButton>
+                        <XButton mini type='primary' onClick={this.doPublish}>发表</XButton>
                     </ToolBar>
                     <div style={_styleUpload}>
-                        <Gallery style={{minHeight:'100px'}}>
-                            <Uploader onSelectImg={this.onSelectImg}/>
+                        <Gallery style={{ minHeight: '100px' }}>
+                            <Uploader onSelectImg={this.props.onSelectImage}/>
                         </Gallery>
                     </div>
                     { _emoticonsPicker }
