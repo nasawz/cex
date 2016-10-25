e97d0cf87fcb526012527fe82646e662f5e34972
王喆 <nasa127@gmail.com>
2016-10-25 12:26:52 +0800

    fix

--- example/modules/cex/views/vpopuppicker.jsx
+++ example/modules/cex/views/vpopuppicker.jsx
@@ -4,93 +4,133 @@ import ItemCell from 'cex/components/list/item-cell.jsx'
 import PopupPicker from 'cex/components/popup-picker/popup-picker.jsx'
 
 const VPopupPicker = React.createClass({
-    getPopupValue(Arr){
-        let val=''
-        for (var i = 0; i < Arr.length; i++) {
-            val=Arr[i]+''
+    getPopupValue(arr) {
+        if (!arr) {
+            return
+        }
+        let val = ''
+        for (var i = 0; i < arr.length; i++) {
+            val = val + arr[i] + ''
         }
         return val
     },
     showPopupPicker(e) {
         let id = e.currentTarget.getAttribute('data-id')
-        this.setState({
-            show: id
-        })
+        this.setState({show: id})
     },
     hidePopupPicker() {
-        this.setState({
-            show: ''
-        })
+        this.setState({show: ''})
     },
-    closePopupPicker1(val){
-        console.log(val,'123');
+    closePopupPicker1(val) {
         this.setState({
-            data1:{
-                value:val
+            data1: {
+                value: val
             }
         })
 
     },
-    closePopupPicker2(val){
+    closePopupPicker2(val) {
         this.setState({
-            data2:{
-                value:val
+            data2: {
+                value: val
             }
         })
-        console.log(val);
     },
     getInitialState() {
         return {
             show: '',
-            data1:{
-                data:
-                    [[{name:'2001年',value:'2001'},{name:'2002年',value:'2002'},{name:'2003年',value:'2003'},
-                    {name:'2004年',value:'2004'},{name:'2005年',value:'2005'},{name:'2009年',value:'2009'},
-                    {name:'2006年',value:'2006'},{name:'2007年',value:'2007'},{name:'2008年',value:'2008'}]],
-                value:['2003'],
-                columns:1,
+            data1: {
+                data: [
+                    [
+                        {
+                            name: '2001年',
+                            value: '2001'
+                        }, {
+                            name: '2002年',
+                            value: '2002'
+                        }, {
+                            name: '2003年',
+                            value: '2003'
+                        }, {
+                            name: '2004年',
+                            value: '2004'
+                        }, {
+                            name: '2005年',
+                            value: '2005'
+                        }, {
+                            name: '2009年',
+                            value: '2009'
+                        }, {
+                            name: '2006年',
+                            value: '2006'
+                        }, {
+                            name: '2007年',
+                            value: '2007'
+                        }, {
+                            name: '2008年',
+                            value: '2008'
+                        }
+                    ]
+                ],
+                value: ['2003'],
+                columns: 0,
+                id: 'data1'
             },
-            data2:{
-                data:
-                [
+            data2: {
+                data: [
                     [
                         '你', '我', '他'
                     ],
-                    [
-                        'you', 'I', 'him'
-                    ],
-
+                    ['you', 'I', 'him']
                 ],
-                value:['你','I'],
-                columns:2,
-                id:2
-
+                value: [
+                    '你', 'I'
+                ],
+                // columns:2,
+                id: 'data2'
             }
         }
     },
 
-    render () {
-        let showT1 = (this.state.show=='t1') ? true : false
-        let showT2 = (this.state.show=='t2') ? true : false
+    render() {
+        let showT1 = (this.state.show == 't1')
+            ? true
+            : false
+        let showT2 = (this.state.show == 't2')
+            ? true
+            : false
 
         return (
             <List>
                 <ItemCell>
-                    <div style={{width:'100%'}}  data-id="t1" onClick={this.showPopupPicker}>
-                        <span style={{float:'left'}}>默认</span>
-                        <span style={{float:'right',color:'#aaa'}}>{this.getPopupValue(this.state.data1.value)}</span>
+                    <div style={{
+                        width: '100%'
+                    }} data-id="t1" onClick={this.showPopupPicker}>
+                        <span style={{
+                            float: 'left'
+                        }}>默认</span>
+                        <span style={{
+                            float: 'right',
+                            color: '#aaa'
+                        }}>{this.getPopupValue(this.state.data1.value)}</span>
                     </div>
                 </ItemCell>
                 <ItemCell>
-                    <div style={{width:'100%'}}  data-id="t2" onClick={this.showPopupPicker}>
-                        <span style={{float:'left'}}>多列</span>
-                        <span style={{float:'right',color:'#aaa'}}>{this.getPopupValue(this.state.data2.value)}</span>
+                    <div style={{
+                        width: '100%'
+                    }} data-id="t2" onClick={this.showPopupPicker}>
+                        <span style={{
+                            float: 'left'
+                        }}>多列</span>
+                        <span style={{
+                            float: 'right',
+                            color: '#aaa'
+                        }}>{this.getPopupValue(this.state.data2.value)}</span>
                     </div>
                 </ItemCell>
 
                 <PopupPicker show={showT1} hidePopupPicker={this.hidePopupPicker} closePopupPicker={this.closePopupPicker1} pickerData={this.state.data1} />
-                <PopupPicker show={showT2} hidePopupPicker={this.hidePopupPicker} closePopupPicker={this.closePopupPicker2} pickerData={this.state.data2} />
-
+                <PopupPicker show={showT2} hidePopupPicker={this.hidePopupPicker} closePopupPicker={this.closePopupPicker2} pickerData={this.state.data2}/>
             </List>
         )
     }
