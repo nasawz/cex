b9e36ccffa9004350f1cde7e4b91fe31ccfcbaf9
xiangxiang <smile78356@foxmail.com>
2016-11-04 18:23:00 +0800

    fix

--- src/complex/card/rank-card.jsx
+++ src/complex/card/rank-card.jsx
@@ -18,8 +18,8 @@ const CommentCard = React.createClass({
 
     getDefaultProps() {
         return {
-            order: 4,
-            user: {
+            data: {
+              rank:0,
                 name: '朱朱',
                 avatar: {
                     img: 'http://shp.qpic.cn/bizmp/bzxzibRQFVkIzjofrT0SOmuI9vZ0kWCJ4BicF1rw5qibVkKuYwiaxJnOPA/',
@@ -27,16 +27,7 @@ const CommentCard = React.createClass({
                     plus: null
                 },
             },
-            txt: '女王的头冠，现在国博展览。',
-            showAllTxt: false,
-            isFavorite: true,
-            favoriteNum: 11,
-            commentNum: 10,
-            gallery: [
-                'http://ces00.b0.upaiyun.com/2016/09/17/upload_6f046a3cba5894c8deb72fd1b568021c.jpg',
-                'http://ces00.b0.upaiyun.com/2016/10/10/upload_cce5c9c2abe8733ec638b8a3285e5de1.jpg',
-                'http://ces00.b0.upaiyun.com/2016/10/09/upload_19e65d722b72075383f6ba3cd7d23e32.jpg',
-            ]
+
         }
     },
     getInitialState() {
@@ -45,7 +36,7 @@ const CommentCard = React.createClass({
         }
     },
     renderAvatar() {
-        let {avatar} = this.props.user
+        let {avatar} = this.props.data
         return (
             <Avatar src={avatar.img}
                 plus={avatar.plus}
@@ -56,6 +47,8 @@ const CommentCard = React.createClass({
         )
     },
     renderOrder() {
+      let {rank} = this.props.data
+      console.log(rank)
         let imgStyle = {
             width: '15px',
             height: '20px',
@@ -71,7 +64,7 @@ const CommentCard = React.createClass({
             justifyContent: 'center',
         }
         let content
-        switch (this.props.order) {
+        switch (rank) {
         case 1:
             content = <img  style={imgStyle} src={require('../../img/icon_award1.png') } />
             break
@@ -82,15 +75,21 @@ const CommentCard = React.createClass({
             content = <img  style={imgStyle} src={require('../../img/icon_award3.png') } />
             break
         default:
-            content = <div style={numStyle}>{this.props.order}</div>
+            content = <div style={numStyle}>{rank}</div>
         }
         return (
             content
         )
-
     },
     render() {
-        let {data} = this.props
+      let {data,filter} = this.props
+      let source
+      if(filter=='exp'){
+        source=data.experience
+      }else if(filter=='gold'){
+        source=data.rewards
+      }
+
         return (
             <Card onClick={this.goUserProfile}>
                 <Item style={{ background: '#fff' }}>
@@ -100,15 +99,15 @@ const CommentCard = React.createClass({
                 </ItemMedia>
                 <ItemContent>
                     <ItemTitleRow>
-                        <ItemTitle style={{ 'color': '#536893' }}>{data.name}</ItemTitle>
-                        <ItemTitleAfter>3</ItemTitleAfter>
+                        <ItemTitle style={{ 'color': '#536893' }}>{data.screen_name}</ItemTitle>
+                        <ItemTitleAfter>{source}</ItemTitleAfter>
                     </ItemTitleRow>
 
                 </ItemContent>
             </Item>
             </Card>
 
-            
+
         )
     }
 })
