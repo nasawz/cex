e5f7811872cc8c250c3f5b5665d22bfe22199e61
吴丹丹 <danw@kailuzhe.cn>
2016-11-14 15:37:45 +0800

    lottery-turntable控件结构

---
+++ src/components/lottery-turntable/lottery-turntable.jsx
@@ -0,0 +1,22 @@
+/**
+ * create by dandan
+ */
+
+import React from 'react'
+import classNames from 'classnames'
+
+import './lottery-turntable.less'
+
+const LotteryTurntable = React.createClass({
+    render() {
+        return (
+            <div>
+               lottery-turntable component
+            </div>
+
+        )
+    }
+
+})
+
+export default LotteryTurntable
