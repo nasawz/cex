d19be356030b1c7d0ad6ec84aed21c8a26560b7e
李继红 <707230595@qq.com>
2016-11-29 18:14:15 +0800

    fix

--- example/modules/cex/views/vreceiverform.jsx
+++ example/modules/cex/views/vreceiverform.jsx
@@ -1,14 +1,46 @@
 import React from 'react'
 import ReceiverForm from 'cex/complex/form/receiver-form.jsx'
+import data from './city-data.js'
 
 const VReceiverForm = React.createClass({
-    handle() {
+    showPopupPicker() {
+        this.setState({
+            show: true
+        })
+    },
+    hidePopupPicker() {
+        this.setState({
+            show: false
+        })
+    },
+    closePopupPicker(val) {
+        this.setState({
+            data: {
+                data: data,
+                id: 'city',
+                value: val,
+                columns: 3
+            }
+        })
+    },
+    handler() {
         console.log(this.refs.form.getValue());
+    },
+    getInitialState() {
+        return {
+            show: false,
+            data: {
+                data: data,
+                id: 'city',
+                value: ['江苏','常州','钟楼区'],
+                columns: 3
+            }
+        }
     },
     render () {
         return (
             <div>
-                <ReceiverForm ref='form' />
+                <ReceiverForm ref='form' show={this.state.show} pickerData={this.state.data} showPopupPicker={this.showPopupPicker} hidePopupPicker={this.hidePopupPicker} closePopupPicker={this.closePopupPicker} />
             </div>
         )
     }
